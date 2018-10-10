c99fd8f8ce4a5356639684150ae2151243f4fc48
anmichalak <wievowu@wp.pl>
2018-10-10 20:13:22 +0200

    skończone zadanie 2

--- cw2.c
+++ cw2.c
@@ -1,7 +1,9 @@
-i#include <stdio.h>
+#include <stdio.h>
 
 int main(){
-
-
-
+ for(int licznik = 0; licznik <= 100; ++licznik) {
+   if (licznik % 3 != 0){   
+     printf("%d\n", licznik);
+   }
+  }
 }
