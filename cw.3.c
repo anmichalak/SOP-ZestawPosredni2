891f3f78433def00e33a846b1420d88607dd5cf0
anmichalak <wievowu@wp.pl>
2018-10-17 19:26:13 +0200

    refactor cw3

--- cw.3.c
+++ cw.3.c
@@ -4,12 +4,9 @@
 int main () {
     int i, j;
  
-    for(i=0; i<=10; ++i, printf("%4s","\n"))
-    for(j=0; j<=10; ++j)
-     if (i&&j) {
-      printf("%4d", i*j);
-     }
-     else { 
-      printf("%4s","");
- }
+    for(i=1; i<=10; ++i, printf("\n")){
+      for(j=1; j<=10; ++j){
+        printf("%4d", i*j);
+      }
+    }
 }
