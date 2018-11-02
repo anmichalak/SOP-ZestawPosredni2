ebc6c83a2c100e82e1bf19f48d6c7c8fa5df6fad
anmichalak <wievowu@wp.pl>
2018-11-02 22:05:44 +0100

    ukonczone cwiczenie 8

--- cw8.c
+++ cw8.c
@@ -1,51 +1,33 @@
 #include<stdio.h>
 
-int toHex(int decimalnumber, int reszta){
-   
-   printf("Enter number to convert:", decimalnumber);
-   scanf("%d", &decimalnumber);
-   int a;  
-   
-     while(decimalnumber>0){
-       reszta  = decimalnumber%16;
-       if( reszta>9){
-          switch (reszta) {
-          case 10:
-             printf("A");
-             break;
-          case 11:
-             printf("B");
-             break;
-          case 12:
-             printf("C");
-             break;
-          case 13:
-              printf("D");
-             break;
-          case 14:
-            printf("E");
-             break;
-          case 15:
-             printf("F");
-             break;
-          case 16:
-            printf("G");
-             break;
-          case 17:
-             printf("H");
-             break;
-          case 18:
-           printf("I");
-             break;
-         }
-       else {
-         printf("%d", reszta);
-       } 
-       decimalnumber = decimalnumber/16;
+int toHex(int dec){
+
+int reszta;
+int index = 0;
+int result[8];
+int iterator;
+
+ while(dec != 0){
+   reszta=dec%16;
+    if(reszta > 9){
+      result[index++] = reszta + 55;
+      dec = dec/16;
      }
-   }
+     else{
+      result[index++]=reszta+48;
+      dec= dec/16;
+     }
+ }
+ for(iterator=index-1;iterator >= 0;iterator--){
+  printf("%c", result[iterator]);
+  }
 }
-int main() {
- int a = toHex;
- printf("Twój wynik to:", a);
-}
+
+
+int main(){
+  int dec;
+  printf("Wpisz liczbe: ", dec);
+  scanf("%d", &dec);
+  toHex(dec);
+
+} 
